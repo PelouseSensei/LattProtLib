5ca62280d7abc9565373e5d369d5facfa6a92d3c
Nicolas Rocquelain <nicolas.rocquelain@espci.fr>
2025-11-28 11:43:01 +0100

    includable lib

---
+++ LattProtLib/include/LattProtLib.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+// Public headers
+#include <lattice/lattice.hpp>
+#include <lattice/boundaryconditions.hpp>
+
+#include <system/configuration.hpp>
+#include <system/snapper.hpp>
+#include <system/occupancygrid.hpp>
+
+#include <moves/moves.hpp>
+
+#include <samplers/mcmc_samplers.hpp>
+#include <samplers/WL_samplers.hpp>
+
+#include <schedulers/task_scheduler.hpp>
+
+// HighFive headers
+#include <highfive/H5File.hpp>
